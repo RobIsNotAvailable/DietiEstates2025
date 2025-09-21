d435699733e9045aa3103742325c00305a77f7e2
Rob <robamendolaa@gmail.com>
2025-09-21 19:25:35 +0200

    springboot base structure

---
+++ src/main/java/com/DD25/DietiDeals25/DietiDeals25Application.java
@@ -0,0 +1,13 @@
+package com.DD25.DietiDeals25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietiDeals25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietiDeals25Application.class, args);
+	}
+
+}
